b25bcc75611c352855ab6dcf9d07c13599ea53ab
Tristan_HHSAalen <71336@studmail.htw-aalen.de>
2020-11-18 16:55:41 +0100

    Moved: Population of PointCloud2 to dedicated function since this will be performed on multiple locations in the code

--- arlo_navigation/src/markfreespace.cpp
+++ arlo_navigation/src/markfreespace.cpp
@@ -1,27 +1,28 @@
+//general includes
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "geometry_msgs/Pose.h"
-#include "sensor_msgs/PointCloud.h"
 #include "geometry_msgs/Quaternion.h"
+#include "geometry_msgs/Point32.h"
 #include "nav_msgs/Odometry.h"
+#include "std_msgs/String.h"
+#include <math.h>
+#include <sstream>
 
-
+//custom message types
 #include "road_detection/LineMatch.h"
 #include "road_detection/Line.h"
 #include "road_detection/LineArray.h"
 #include "road_detection/Road.h"
 
-
+//PCL Stuff
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
-//#include <pcl/point_types.h>
 
-#include <math.h>
 
-#include <sstream>
+
 
 //TODO dyn_recon
 double searchradius=1000; //distance in mm
@@ -31,72 +32,119 @@ geometry_msgs::Point position;
 
 //Thresholds for slam data evaluation everything between unknown thresh and known thresh gets treated as empty space
 //TODO make these configurable using dyn_recon
-double unknownThresh;
-double knownThresh;
+//double unknownThresh;
+//double knownThresh;
+
+bool MiddlePoints;
 
 ros::Publisher pub;
+sensor_msgs::PointCloud2 constructcloud(std::vector<geometry_msgs::Point32> &points, float intensity, std::string frame);
 
 class Listener
 	{
 	public:
-		void roadCallback(const road_detection::Road& road)
+		void roadCallback(const road_detection::RoadConstPtr& road)
 		{
+			//Transforming points of road_detection into sensor_msgs::PointCloud2 so they can be used in the costmap
+			std::vector<geometry_msgs::Point32> points;
 
-			geometry_msgs::Point32 left_line[]= road.lineLeft.points;
-			constructcloud(left_line, 100, "base_footprint");
+			int i;
+
+			//publishing borders and middle line individual to give better flexibility
+			for(i=0;i<road->lineLeft.points.size();i++)
+			{
+				points.push_back(road->lineLeft.points[i]);
+			}
+			for(i=0;i<road->lineRight.points.size();i++)
+			{
+
+				points.push_back(road->lineRight.points[i]);
+			}
+			pub.publish(constructcloud(points,100,"base_footprint"));
+			points.clear();
+
+			//TODO dyn recon
+			if(MiddlePoints){
+				for(i=0;i<road->lineMiddle.points.size();i++)
+				{
+					points.push_back(road->lineMiddle.points[i]);
+				}
+				pub.publish(constructcloud(points,100,"base_footprint"));
+				points.clear();
+			}
+
 		};
 
 
 	public:
-		void mapCallback(const nav_msgs::OccupancyGrid& grid)
+		void mapCallback(const nav_msgs::OccupancyGridConstPtr& grid)
 		{
+			//Transforming points of slam map into sensor_msgs::PointCloud2 so they can be used in the costmap to have costs for points we have seen before but not atm
+			int map_height=grid->info.height;
+			int map_width=grid->info.width;
 
-			int map_height=grid.info.height;
-			int map_width=grid.info.width;
 			int pixel_x;
 			int pixel_y;
-			double Distance;
+			double coord_x;
+			double coord_y;
 
-			for (int i=0; i<grid.data.size(); i++)
+			//resolution in m/cell
+			double resolution = grid->info.resolution;
+			double Distance;
+			std::vector<geometry_msgs::Point32> points;
+			geometry_msgs::Point32 currentpoint;
+
+			for (int i=0; i<grid->data.size(); i++)
 			{
 
 				//get pixel coordinate relative to map origin
-				pixel_x=grid.info.origin.position.x-(i%grid.info.width);
-				pixel_y=grid.info.origin.position.y-((int)floor(i/grid.info.width));
+				pixel_x=grid->info.origin.position.x-(i%map_width);
+				pixel_y=grid->info.origin.position.y-((int)floor(i/map_width));
 
-				//calc Distance between Arlo and given Pixel (assumption map origin=robot pos reference)
-				Distance=sqrt(pow(abs(pixel_x-position.x),2.0)+pow(abs(pixel_x-position.x),2.0));
-				if(grid.data.at(i)>0&&grid.data.at(i)<100)
+				currentpoint.x=pixel_x*resolution;
+				currentpoint.y=pixel_y*resolution;
+				currentpoint.z=0;
+
+				if(grid->data.at(i)>=50)
 				{
-					ROS_INFO("%d",grid.data.at(i));
-
+					points.push_back(currentpoint);
 				}
 
+				//calc Distance between Arlo and given Pixel (assumption map origin=robot pos reference)
+//				Distance=sqrt(pow(abs(pixel_x-position.x),2.0)+pow(abs(pixel_x-position.x),2.0));
+
+//				if(Distance<searchradius)
+//				{
+//					currentpoint.x=grid->info.resolution;
+//					points.push_back(currentpoint);
+//				}
 			}
+			pub.publish(constructcloud(points,100,"map"));
+
 		};
 	public:
-		void odomCallback(const nav_msgs::Odometry& odom)
+		void odomCallback(const nav_msgs::OdometryConstPtr& odom)
 		{
+			//get current position
 
-			position=odom.pose.pose.position;
-			orientation=odom.pose.pose.orientation;
+			position=odom->pose.pose.position;
+			orientation=odom->pose.pose.orientation;
 		};
 
 	};
 
 
 
-void constructcloud(geometry_msgs::Point32 points[], int intensity, char frame_id[])
+sensor_msgs::PointCloud2 constructcloud(std::vector<geometry_msgs::Point32> &points, float intensity, std::string frame)
 	{
-	//Construct PointCloud2 Message
+
+	//Construct PointCloud2 Message with given array of points tf_frame and intensity
 
 		int Count=sizeof(points);
-		int borderintensity=100;
-		int middleintensity=100;
 		// Populate message fields
 		const uint32_t POINT_STEP = 16;
 		sensor_msgs::PointCloud2 msg;
-		msg.header.frame_id = frame_id;
+		msg.header.frame_id =frame;
 		msg.header.stamp = ros::Time::now();
 		msg.fields.resize(4);
 		msg.fields[0].name = "x";
@@ -136,7 +184,7 @@ void constructcloud(geometry_msgs::Point32 points[], int intensity, char frame_i
 		msg.is_bigendian = false;
 		msg.is_dense = true; //maybe invalid points
 		msg.data.resize(msg.row_step); // Shrink to actual size
-		pub.publish(msg);
+		return msg;
 	}
 
 
