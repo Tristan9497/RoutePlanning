db8cb161fff4330aee3ca5d9327d0474af11f0fc
Tristan_HHSAalen <71336@studmail.htw-aalen.de>
2020-12-02 19:24:01 +0100

    added pposeefinder

--- arlo_navigation/src/posefinder.cpp
+++ arlo_navigation/src/posefinder.cpp
@@ -1,8 +1,20 @@
 #include <ros/ros.h>
 
+
+//TF
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Twist.h>
+
+
+
 #include <math.h>
+#include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Point32.h"
+#include "sensor_msgs/LaserScan.h"
+#include "nav_msgs/OccupancyGrid.h"
+#include "nav_msgs/Odometry.h"
 //custom message types
 #include "road_detection/LineMatch.h"
 #include "road_detection/Line.h"
@@ -14,14 +26,25 @@
 #include <dynamic_reconfigure/server.h>
 #include <arlo_navigation/posefinderConfig.h>
 
+
+
 double searchradius=5;
+double searchangle=60*M_PI/180;
+double costthreshold=100;
 bool Rightside;
 ros::Publisher pub;
-road_detection::Line Lane;
+
+std::vector<geometry_msgs::Point32> scan;
+geometry_msgs::TransformStamped transformStamped;
+geometry_msgs::Pose currentpose;
+void publish_pose(road_detection::LineConstPtr& Lane);
 
 void callback(arlo_navigation::posefinderConfig &config, uint32_t level) {
 	Rightside=config.Side;
-	ROS_INFO("Side",Rightside);
+	ROS_INFO("%d",Rightside);
+	searchradius=config.Searchradius;
+	searchangle=config.Searchangle;
+	costthreshold=config.Costthreshold;
 }
 
 
@@ -30,32 +53,38 @@ class Listener
 	public:
 		void roadCallback(const road_detection::RoadConstPtr& road)
 		{
-
-			double mindist;
 			double x;
 			double y;
 			double range;
-			mindist=searchradius;
+			double angle;
 			int j;
+			int size;
 			tf2::Quaternion myQuaternion;
 			geometry_msgs::PoseStamped newpose;
-			//publishing borders and middle line individual to give better flexibility
-			if(Rightside){
-				Lane=road->laneRight;
+
+
+			if(Rightside)
+			{
+				j=road->laneRight.points.size()-1;
+				angle=atan((road->laneRight.points.at(j).y-road->laneRight.points.at(j-1).y)/(road->laneRight.points.at(j).x-road->laneRight.points.at(j-1).x));
+				newpose.pose.position.x=road->laneRight.points.at(j).x;
+				newpose.pose.position.y=road->laneRight.points.at(j).y;
+				size=road->laneRight.points.size();
 			}
 			else
 			{
-				Lane=road->laneLeft;
+				j=road->laneLeft.points.size()-1;
+				angle=atan((road->laneLeft.points.at(j).y-road->laneLeft.points.at(j-1).y)/(road->laneLeft.points.at(j).x-road->laneLeft.points.at(j-1).x));
+				newpose.pose.position.x=road->laneLeft.points.at(j).x;
+				newpose.pose.position.y=road->laneLeft.points.at(j).y;
+				size=road->laneLeft.points.size();
 			}
-			if(Lane.points.size()>0){
-				j=Lane.points.size()-1;
 
-				double angle=atan((Lane.points.at(j).y-Lane.points.at(j-1).y)/(Lane.points.at(j).x-Lane.points.at(j-1).x));
+			if(size>0){
+
 				newpose.header.seq = 1;
 				newpose.header.frame_id="base_footprint";
 				newpose.header.stamp = ros::Time::now();
-				newpose.pose.position.x=Lane.points.at(j).x;
-				newpose.pose.position.y=Lane.points.at(j).y;
 				newpose.pose.position.z=0;
 				myQuaternion.setRPY( 0, 0, angle );
 				newpose.pose.orientation.x=myQuaternion.getX();
@@ -65,32 +94,65 @@ class Listener
 				pub.publish(newpose);
 			}
 
+
 		};
+	public:
+		void scanCallback(const sensor_msgs::LaserScanConstPtr& Scan)
+		{
+			geometry_msgs::Point32 scanpoint;
+			scan.clear();
+			double range,angle,minangle,maxangle;
+			minangle=M_PI-(searchangle/2);
+			maxangle=M_PI+(searchangle/2);
+			for(int i=0;i<Scan->ranges.size();i++)
+				{
+					range=Scan->ranges.at(i);
+					angle=(i*Scan->angle_increment)+Scan->angle_min;
+
+					if((range<searchradius)&&(angle>minangle)&&(angle<maxangle)){
+
+						//offset value from arlo_2stack urdf
+
+						scanpoint.x=range*cos(angle)+0.175;
+						scanpoint.y=range*sin(angle);
+						scanpoint.z=0.0;
+
+						scan.push_back(scanpoint);
+					}
+
+				}
+		};
+
+
 	};
+
+
 
 int main(int argc, char* argv[])
 {
   // This must be called before anything else ROS-related
 	ros::init(argc, argv, "posefinder");
 	ros::NodeHandle n;
-
+	Listener listener;
 
 	dynamic_reconfigure::Server<arlo_navigation::posefinderConfig> server;
 	dynamic_reconfigure::Server<arlo_navigation::posefinderConfig>::CallbackType f;
-
+	ros::Subscriber road = n.subscribe("/roadDetection/road", 1000, &Listener::roadCallback, &listener);
+	ros::Subscriber scan = n.subscribe("/base_scan", 1000, &Listener::scanCallback, &listener);
 	f = boost::bind(&callback, _1, _2);
 	server.setCallback(f);
 
+	ros::Rate r=0.5;
 
-	Listener listener;
-	ros::Rate r(0.5);
-	ros::Subscriber road = n.subscribe("/roadDetection/road", 1000, &Listener::roadCallback, &listener);
 	while(ros::ok()){
+
 		pub = n.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1000);
 		//TODO maybe 3 different point clouds for each line of the track
 
 		ros::spinOnce();
 		r.sleep();
+
 	}
 	return 0;
 }
+
