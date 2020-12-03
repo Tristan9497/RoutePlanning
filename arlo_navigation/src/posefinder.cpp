0d465cd99e827571a8d80ec5115b5e025792792e
Tristan_HHSAalen <71336@studmail.htw-aalen.de>
2020-12-03 18:52:11 +0100

    fixed posefinder
    added buffer for roaddetection to prevent data loss
    added functionality for lane swap
    Fixed roadangle compensation
    added fullstop when road is blocked

--- arlo_navigation/src/posefinder.cpp
+++ arlo_navigation/src/posefinder.cpp
@@ -1,12 +1,12 @@
 #include <ros/ros.h>
 
-
 //TF
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
 
-
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 
 #include <math.h>
 #include "geometry_msgs/Pose.h"
@@ -32,69 +32,158 @@ double searchradius=5;
 double searchangle=60*M_PI/180;
 double costthreshold=100;
 bool Rightside;
+bool goaltrigger=false;
+double roadangle;
+move_base_msgs::MoveBaseGoal goal;
 ros::Publisher pub;
 
 std::vector<geometry_msgs::Point32> scan;
 geometry_msgs::TransformStamped transformStamped;
 geometry_msgs::Pose currentpose;
-void publish_pose(road_detection::LineConstPtr& Lane);
+
+//storing the latest road data for data loss cases
+road_detection::Line LeftLine;
+road_detection::Line RightLine;
+road_detection::Line MiddleLine;
+road_detection::Line LeftLane;
+road_detection::Line RightLane;
+
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+//should be reference???
+double polynomial(double x, road_detection::Line::_polynomial_type::_a_type a) {
+	double xp = 1;
+	double result = 0;
+
+	for ( int i = 0; i < a.size(); i++ ) {
+		result += xp * a[i];
+		xp *= x;
+	}
+	return result;
+}
+double aproxroadangle(void)
+	{
+	//approximate the road width based on the angle of the road relative to the robots direction
+	//TODO check reliability
+		double dx=	((LeftLine.points.at(1).x-LeftLine.points.at(0).x)+
+					(RightLine.points.at(1).x-RightLine.points.at(0).x)+
+					(MiddleLine.points.at(1).x-MiddleLine.points.at(0).x)+
+					(LeftLane.points.at(1).x-LeftLane.points.at(0).x)+
+					(RightLane.points.at(1).x-RightLane.points.at(0).x))/5;
+		double dy=	((LeftLine.points.at(1).y-LeftLine.points.at(0).y)+
+					(RightLine.points.at(1).y-RightLine.points.at(0).y)+
+					(MiddleLine.points.at(1).y-MiddleLine.points.at(0).y)+
+					(LeftLane.points.at(1).y-LeftLane.points.at(0).y)+
+					(RightLane.points.at(1).y-RightLane.points.at(0).y))/5;
+
+		return atan(dy/dx);
+	}
 
 void callback(arlo_navigation::posefinderConfig &config, uint32_t level) {
 	Rightside=config.Side;
-	ROS_INFO("%d",Rightside);
 	searchradius=config.Searchradius;
 	searchangle=config.Searchangle;
 	costthreshold=config.Costthreshold;
 }
-
+void constructgoal(road_detection::Line &Line)
+	{
+		tf2::Quaternion myQuaternion;
+		int j,size;
+		float angle,x,y;
+		goaltrigger=true;
+		j=Line.points.size()-1;
+		angle=atan((Line.points.at(j).y-Line.points.at(j-1).y)/(Line.points.at(j).x-Line.points.at(j-1).x));
+		x=Line.points.at(j).x;
+		y=Line.points.at(j).y;
+		size=Line.points.size();
+		ROS_INFO("x:%f,y:%f",x,y);
+		goal.target_pose.header.frame_id="base_footprint";
+		goal.target_pose.header.stamp = ros::Time::now();
+		goal.target_pose.pose.position.x=x;
+		goal.target_pose.pose.position.y=y;
+		goal.target_pose.pose.position.z=0;
+		myQuaternion.setRPY( 0, 0, angle );
+		myQuaternion=myQuaternion.normalized();
+		goal.target_pose.pose.orientation.x=myQuaternion.getX();
+		goal.target_pose.pose.orientation.y=myQuaternion.getY();
+		goal.target_pose.pose.orientation.z=myQuaternion.getZ();
+		goal.target_pose.pose.orientation.w=myQuaternion.getW();
+	}
 
 class Listener
 	{
 	public:
 		void roadCallback(const road_detection::RoadConstPtr& road)
 		{
-			double x;
-			double y;
-			double range;
-			double angle;
-			int j;
-			int size;
-			tf2::Quaternion myQuaternion;
-			geometry_msgs::PoseStamped newpose;
+			bool obstacleright=false;
+			bool obstacleleft=false;
 
-
-			if(Rightside)
+			//DataSync
+			if(road->laneLeft.points.size()>0)
 			{
-				j=road->laneRight.points.size()-1;
-				angle=atan((road->laneRight.points.at(j).y-road->laneRight.points.at(j-1).y)/(road->laneRight.points.at(j).x-road->laneRight.points.at(j-1).x));
-				newpose.pose.position.x=road->laneRight.points.at(j).x;
-				newpose.pose.position.y=road->laneRight.points.at(j).y;
-				size=road->laneRight.points.size();
+				LeftLane=road->laneLeft;
 			}
-			else
+			if(road->laneRight.points.size()>0)
 			{
-				j=road->laneLeft.points.size()-1;
-				angle=atan((road->laneLeft.points.at(j).y-road->laneLeft.points.at(j-1).y)/(road->laneLeft.points.at(j).x-road->laneLeft.points.at(j-1).x));
-				newpose.pose.position.x=road->laneLeft.points.at(j).x;
-				newpose.pose.position.y=road->laneLeft.points.at(j).y;
-				size=road->laneLeft.points.size();
+				RightLane=road->laneRight;
 			}
 
-			if(size>0){
+			if(road->lineLeft.points.size()>0)
+			{
+				LeftLine=road->lineLeft;
+			}
+			if(road->lineRight.points.size()>0)
+			{
+				RightLine=road->lineRight;
+			}
+			if(road->lineMiddle.points.size()>0)
+			{
+				MiddleLine=road->lineMiddle;
+			}
+			roadangle=aproxroadangle();
 
-				newpose.header.seq = 1;
-				newpose.header.frame_id="base_footprint";
-				newpose.header.stamp = ros::Time::now();
-				newpose.pose.position.z=0;
-				myQuaternion.setRPY( 0, 0, angle );
-				newpose.pose.orientation.x=myQuaternion.getX();
-				newpose.pose.orientation.y=myQuaternion.getY();
-				newpose.pose.orientation.z=myQuaternion.getZ();
-				newpose.pose.orientation.w=myQuaternion.getW();
-				pub.publish(newpose);
+			road_detection::Line Line;
+			for(int i=0; i<scan.size();i++){
+				//check whether the point is in the lane by projecting it on to the 3 roadlines and comparing their y values
+				//exact calculation not necessary since road makes wide curves
+				if(polynomial(scan.at(i).x, LeftLine.polynomial.a)>scan.at(i).y&&polynomial(scan.at(i).x, MiddleLine.polynomial.a)<scan.at(i).y)
+				{
+					obstacleleft=true;
+				}
+				else if(polynomial(scan.at(i).x, RightLine.polynomial.a)<scan.at(i).y&&polynomial(scan.at(i).x, MiddleLine.polynomial.a)>scan.at(i).y)
+				{
+					obstacleright=true;
+				}
+
 			}
 
 
+			if(obstacleleft&&!obstacleright)
+			{
+				ROS_INFO("LeftLane Blocked");
+				Line=RightLane;
+				constructgoal(Line);
+			}
+			else if(obstacleleft&&obstacleright)
+			{
+				ROS_INFO("Road Blocked");
+				goaltrigger=false;
+				obstacleleft=false;
+				obstacleright=false;
+			}
+			else if((!obstacleleft&&obstacleright)||!Rightside)
+			{
+				ROS_INFO("RightLane Blocked");
+				Line=LeftLane;
+				constructgoal(Line);
+			}
+
+			else if(!obstacleright||!obstacleleft){
+				ROS_INFO("Road Free");
+				Line=RightLane;
+				constructgoal(Line);
+			}
 		};
 	public:
 		void scanCallback(const sensor_msgs::LaserScanConstPtr& Scan)
@@ -102,28 +191,21 @@ class Listener
 			geometry_msgs::Point32 scanpoint;
 			scan.clear();
 			double range,angle,minangle,maxangle;
-			minangle=M_PI-(searchangle/2);
-			maxangle=M_PI+(searchangle/2);
+			minangle=M_PI-(searchangle/2)+roadangle;
+			maxangle=M_PI+(searchangle/2)+roadangle;
 			for(int i=0;i<Scan->ranges.size();i++)
 				{
 					range=Scan->ranges.at(i);
 					angle=(i*Scan->angle_increment)+Scan->angle_min;
-
 					if((range<searchradius)&&(angle>minangle)&&(angle<maxangle)){
-
 						//offset value from arlo_2stack urdf
-
 						scanpoint.x=range*cos(angle)+0.175;
 						scanpoint.y=range*sin(angle);
 						scanpoint.z=0.0;
-
 						scan.push_back(scanpoint);
 					}
-
 				}
 		};
-
-
 	};
 
 
@@ -132,8 +214,11 @@ int main(int argc, char* argv[])
 {
   // This must be called before anything else ROS-related
 	ros::init(argc, argv, "posefinder");
+
+
 	ros::NodeHandle n;
 	Listener listener;
+	MoveBaseClient ac("move_base", true);
 
 	dynamic_reconfigure::Server<arlo_navigation::posefinderConfig> server;
 	dynamic_reconfigure::Server<arlo_navigation::posefinderConfig>::CallbackType f;
@@ -142,11 +227,29 @@ int main(int argc, char* argv[])
 	f = boost::bind(&callback, _1, _2);
 	server.setCallback(f);
 
+
+	while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
 	ros::Rate r=0.5;
 
 	while(ros::ok()){
+		if(goaltrigger)
+		{
+			ac.sendGoal(goal);
+			ROS_INFO("goal send");
 
-		pub = n.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1000);
+		}
+		else
+		{
+			ac.cancelAllGoals();
+		}
+//					ac.waitForResult();
+//					if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+//						ROS_INFO("Hooray, the base moved 1 meter forward");
+//					else
+//						ROS_INFO("The base failed to move forward 1 meter for some reason");
+
 		//TODO maybe 3 different point clouds for each line of the track
 
 		ros::spinOnce();
