ff428c8ac8fb9d6a9e16ccd6843987fd6dbad909
Tristan_HHSAalen <71336@studmail.htw-aalen.de>
2021-01-15 19:03:53 +0100

    small changes to trigger left lane service needs fixed tf transforms look at simple layers

--- arlo_navigation/src/posefinder.cpp
+++ arlo_navigation/src/posefinder.cpp
@@ -2,6 +2,7 @@
 #include "nav_msgs/Odometry.h"
 
 //TF
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
@@ -26,7 +27,8 @@
 
 #include <dynamic_reconfigure/server.h>
 #include <arlo_navigation/posefinderConfig.h>
-
+//Services
+#include "arlo_navigation/clearleftlane.h"
 
 
 double searchradius=5;
@@ -34,15 +36,14 @@ double searchangle=60*M_PI/180;
 double costthreshold=100;
 bool Rightside;
 bool goaltrigger=false;
+bool removeblockage=false;
 double roadangle;
 double robot_diameter=0.4;
 
 
 move_base_msgs::MoveBaseGoal goal;
 ros::Publisher pub;
-
 std::vector<geometry_msgs::Point32> scan;
-geometry_msgs::TransformStamped transformStamped;
 geometry_msgs::Pose currentpose;
 
 //storing the latest road data for data loss cases
@@ -51,7 +52,7 @@ road_detection::Line RightLine;
 road_detection::Line MiddleLine;
 road_detection::Line LeftLane;
 road_detection::Line RightLane;
-
+geometry_msgs::Point getPointTransform(std::string frame, ros::Time &Time,geometry_msgs::Point &Point);
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -115,6 +116,38 @@ void constructgoal(road_detection::Line &Line)
 		goal.target_pose.pose.orientation.w=myQuaternion.getW();
 	}
 
+geometry_msgs::Point getPointTransform(std::string frame, ros::Time &Time)
+	{
+
+	tf2_ros::Buffer tfBuffer;
+	tf2_ros::TransformListener tfListener(tfBuffer);
+	geometry_msgs::TransformStamped transform;
+	geometry_msgs::Point PointT;
+	try{
+	  transform = tfBuffer.lookupTransform(frame,frame,Time);
+	  PointT.x=transform.transform.translation.x;
+	  PointT.y=transform.transform.translation.y;
+	  PointT.z=transform.transform.translation.z;
+	}
+	catch (tf2::TransformException &ex) {
+	  ROS_WARN("%s",ex.what());
+	  ros::Duration(1.0).sleep();
+	}
+//	//get transformed point of different time in same frame
+//		tf2_ros::Buffer tfBuffer;
+//		tf2_ros::TransformListener tfListener(tfBuffer);
+//		geometry_msgs::TransformStamped transformStamped;
+//		geometry_msgs::Point PointT;
+//		try{
+//		  transformStamped = tfBuffer.lookupTransform(frame,frame,Time);
+//		  tf2::doTransform(Point, PointT, transformStamped);
+//		}
+//		catch (tf2::TransformException &ex) {
+//		  ROS_WARN("%s",ex.what());
+//		  ros::Duration(1.0).sleep();
+//		}
+		return PointT;
+	}
 class Listener
 	{
 	public:
@@ -126,6 +159,10 @@ class Listener
 			float xobsleftmax;
 			float xobsrightmin=searchradius;
 			float xobsrightmax;
+			geometry_msgs::Point Scan;
+			geometry_msgs::Point LeftPoint;
+			geometry_msgs::Point MiddlePoint;
+			geometry_msgs::Point RightPoint;
 
 			//DataSync
 			if(road->laneLeft.points.size()>0)
@@ -151,23 +188,48 @@ class Listener
 			}
 			roadangle=aproxroadangle();
 
+
 			road_detection::Line Line;
 			for(int i=0; i<scan.size();i++){
+//				Scan.x=scan.at(i).x;
+//				Scan.y=scan.at(i).y;
+//				Scan.z=scan.at(i).z;
+				//get transformed point for every point relative to the latest occurance of the line
+//				LeftPoint =getPointTransform("odom",LeftLine.header.stamp,Scan);
+//				MiddlePoint =getPointTransform("odom",MiddleLine.header.stamp,Scan);
+//				RightPoint =getPointTransform("odom",RightLine.header.stamp,Scan);
+
+
+				geometry_msgs::Point LTrans=getPointTransform("base_link",LeftLine.header.stamp);
+				geometry_msgs::Point RTrans=getPointTransform("base_link",LeftLine.header.stamp);
+				geometry_msgs::Point MTrans=getPointTransform("base_link",LeftLine.header.stamp);
+
 				if(scan.at(i).x>0){
+					ros::Time test = ros::Time::now() - ros::Duration(1.0);
+					geometry_msgs::PointStamped Test;
+					Test.header.stamp=ros::Time::now();
+					Test.header.frame_id="base_link";
+					Test.point.x=scan.at(1).x;
+					Test.point.y=scan.at(1).y;
+					pub.publish(Test);
+					Test.point.x-=getPointTransform("base_link",test).x;
+					Test.point.y-=getPointTransform("base_link",test).y;
+					pub.publish(Test);
 					//check whether the point is in the lane by projecting it on to the 3 roadlines and comparing their y values
 					//exact calculation not necessary since road makes wide curves
-					if(polynomial(scan.at(i).x, LeftLine.polynomial.a)>scan.at(i).y&&polynomial(scan.at(i).x, MiddleLine.polynomial.a)<scan.at(i).y)
+					if(polynomial(LeftPoint.x+LTrans.x, LeftLine.polynomial.a)>LeftPoint.y+LTrans.y&&polynomial(scan.at(i).x+MTrans.x, MiddleLine.polynomial.a)<MiddlePoint.y+MTrans.y)
 					{
 						obstacleleft=true;
 
-						if(scan.at(i).x<xobsleftmin)xobsleftmin=scan.at(i).x;
-						if(scan.at(i).x>xobsleftmax)xobsleftmax=scan.at(i).x;
+						if(LeftPoint.x+LTrans.x<xobsleftmin)xobsleftmin=LeftPoint.x+LTrans.x;
+						if(LeftPoint.x+LTrans.x>xobsleftmax)xobsleftmax=LeftPoint.x+LTrans.x;
 					}
-					else if(polynomial(scan.at(i).x, RightLine.polynomial.a)<scan.at(i).y&&polynomial(scan.at(i).x, MiddleLine.polynomial.a)>scan.at(i).y)
+					else if(polynomial(RightPoint.x+RTrans.x, RightLine.polynomial.a)<RightPoint.y+RTrans.y&&polynomial(MiddlePoint.x+MTrans.x, MiddleLine.polynomial.a)>MiddlePoint.y+MTrans.x)
 					{
 						obstacleright=true;
-						if(scan.at(i).x<xobsrightmin)xobsrightmin=scan.at(i).x;
-						if(scan.at(i).x<xobsrightmax)xobsrightmax=scan.at(i).x;
+
+						if(RightPoint.x+RTrans.x<xobsrightmin)xobsrightmin=RightPoint.x+RTrans.x;
+						if(RightPoint.x+RTrans.x>xobsrightmax)xobsrightmax=RightPoint.x+RTrans.x;
 					}
 				}
 
@@ -178,42 +240,45 @@ class Listener
 			{
 				ROS_INFO("LeftLane Blocked");
 				Line=RightLane;
-				constructgoal(Line);
+				removeblockage=false;
+//				constructgoal(Line);
 			}
 
 			//chicanes
-			else if(obstacleleft&&obstacleright&&(xobsleftmin>(xobsrightmax+robot_diameter)))
-			{
-				ROS_INFO("Road Blocked chicane left to right");
-				Line=RightLane;
-				constructgoal(Line);
-			}
-			else if(obstacleleft&&obstacleright&&(xobsrightmin>(xobsleftmax+robot_diameter)))
-			{
-				ROS_INFO("Road Blocked chicane right to left");
-				Line=LeftLane;
-				constructgoal(Line);
-			}
+//			else if(obstacleleft&&obstacleright&&(xobsleftmin>(xobsrightmax+robot_diameter)))
+//			{
+//				ROS_INFO("Road Blocked chicane left to right");
+//				Line=RightLane;
+////				constructgoal(Line);
+//			}
+//			else if(obstacleleft&&obstacleright&&(xobsrightmin>(xobsleftmax+robot_diameter)))
+//			{
+//				ROS_INFO("Road Blocked chicane right to left");
+//				Line=LeftLane;
+////				constructgoal(Line);
+//			}
 
 			//fullblock
-			else if(obstacleleft&&obstacleright&&(((xobsrightmax-xobsleftmin)<robot_diameter)||((xobsleftmax-xobsrightmin)<robot_diameter)))
-			{
-				ROS_INFO("Road Blocked");
-				goaltrigger=false;
-				obstacleleft=false;
-				obstacleright=false;
-			}
+//			else if(obstacleleft&&obstacleright&&(((xobsrightmax-xobsleftmin)<robot_diameter)||((xobsleftmax-xobsrightmin)<robot_diameter)))
+//			{
+//				ROS_INFO("Road Blocked");
+//				goaltrigger=false;
+//				obstacleleft=false;
+//				obstacleright=false;
+//			}
 			else if((!obstacleleft&&obstacleright)||!Rightside)
 			{
 				ROS_INFO("RightLane Blocked");
 				Line=LeftLane;
-				constructgoal(Line);
+				removeblockage=true;
+//				constructgoal(Line);
 			}
 
 			else if(!obstacleright||!obstacleleft){
 				ROS_INFO("Road Free");
+				removeblockage=false;
 				Line=RightLane;
-				constructgoal(Line);
+//				constructgoal(Line);
 			}
     };
 	public:
@@ -233,6 +298,7 @@ class Listener
 						scanpoint.x=range*cos(angle)+0.175;
 						scanpoint.y=range*sin(angle);
 						scanpoint.z=0.0;
+
 						scan.push_back(scanpoint);
 					}
 				}
@@ -253,10 +319,18 @@ int main(int argc, char* argv[])
 
 	dynamic_reconfigure::Server<arlo_navigation::posefinderConfig> server;
 	dynamic_reconfigure::Server<arlo_navigation::posefinderConfig>::CallbackType f;
+	pub=n.advertise<geometry_msgs::PointStamped>("TestPoint", 1000);
 	ros::Subscriber road = n.subscribe("/roadDetection/road", 1000, &Listener::roadCallback, &listener);
-	ros::Subscriber scan = n.subscribe("/base_scan", 1000, &Listener::scanCallback, &listener);
+	ros::Subscriber scan = n.subscribe("/scan_filtered", 1000, &Listener::scanCallback, &listener);
 	f = boost::bind(&callback, _1, _2);
 	server.setCallback(f);
+
+	//service
+	ros::ServiceClient client = n.serviceClient<arlo_navigation::clearleftlane>("clearblockage");
+	arlo_navigation::clearleftlaneRequest req;
+	arlo_navigation::clearleftlaneResponse res;
+
+
 
 
 	while(!ac.waitForServer(ros::Duration(5.0))){
@@ -264,23 +338,33 @@ int main(int argc, char* argv[])
 	}
 	ros::Rate r=1;
 
-	while(ros::ok()){
-		if(goaltrigger)
-		{
-			ac.sendGoal(goal);
-			ROS_INFO("goal send");
+	while(n.ok()){
 
+		//switching left lane on and off
+		if(removeblockage)
+		{
+			req.trigger=true;
+			client.call(req,res);
 		}
 		else
 		{
-			ac.cancelAllGoals();
+			req.trigger=false;
+			client.call(req,res);
 		}
 
-		ac.waitForResult();
-		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-			ROS_INFO("Hooray, the base moved 1 meter forward");
-		else
-			ROS_INFO("The base failed to move forward 1 meter for some reason");
+//		ac.waitForResult(ros::Duration(5));
+//		if(goaltrigger)
+//		{
+//			ac.sendGoal(goal);
+//
+//
+//			ROS_INFO("goal send");
+//
+//		}
+//		else
+//		{
+//			ac.cancelAllGoals();
+//		}
 
     //TODO maybe 3 different point clouds for each line of the track
 		ros::spinOnce();
