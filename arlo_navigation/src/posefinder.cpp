0f841ac2f5a0049c1aa659e6ffdab99459eef8db
Tristan_HHSAalen <71336@studmail.htw-aalen.de>
2021-01-21 15:26:08 +0100

    Moved Blockage Check and circle aprox to seperate function so they will be called at a fixed rate

--- arlo_navigation/src/posefinder.cpp
+++ arlo_navigation/src/posefinder.cpp
@@ -50,6 +50,7 @@ double furthestpoint;
 move_base_msgs::MoveBaseGoal goal;
 ros::Publisher pub;
 ros::Publisher marker_pub;
+ros::Publisher marker_pub2;
 std::vector<geometry_msgs::PointStamped> scan;
 geometry_msgs::Pose currentpose;
 visualization_msgs::Marker marker;
@@ -59,8 +60,9 @@ road_detection::Line RightLine;
 road_detection::Line MiddleLine;
 road_detection::Line LeftLane;
 road_detection::Line RightLane;
-geometry_msgs::Point getPointTransform(std::string frame, ros::Time &Time,geometry_msgs::Point &Point);
+
 std::string scanframe;
+std::string roadframe;
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 //should be reference???
@@ -74,6 +76,166 @@ double polynomial(double x, road_detection::Line::_polynomial_type::_a_type a) {
 	}
 	return result;
 }
+void lastsquarecircle(road_detection::Line &Line,double &xc,double &yc, double &r)
+
+	{
+		//least square circle approximation as discribed by Randy Bullock in his Least-Squares Circle Fit Paper
+		//takes a Point32 array and calculates the best fitting circle in x,y dimensions, returns the circle parameters xc,yc,r
+		//used to predict the upcoming road that cannot be seen by the camera
+
+		MatrixXd m(2,2);
+		Vector2d b;
+		Vector2d x;
+		//define size so we don't have to do this all the time
+		int size=Line.points.size();
+		//defining sums
+		double Suuu,Svvv,Suvv,Svuu,Suv,Suu,Svv=0;
+
+		//defining averages and parameters u,v that are the point coordinates relative to the average
+		double u,v,xa,ya=0;
+		//first the averages will be build
+		for (int i=0;i<size;i++)
+		{
+			xa+=Line.points.at(i).x;
+			ya+=Line.points.at(i).y;
+		}
+		xa=xa/size;
+		ya=ya/size;
+
+		//new the Sums need to be calculated
+		for (int i=0;i<size;i++)
+		{
+			u=Line.points.at(i).x-xa;
+			v=Line.points.at(i).y-ya;
+
+			Suu+=u*u;
+			Svv+=v*v;
+			Suv+=u*v;
+			Suvv+=u*v*v;
+			Svuu+=v*u*u;
+			Suuu+=u*u*u;
+			Svvv+=v*v*v;
+		}
+		m(0,0)=Suu;
+		m(0,1)=Suv;
+		m(1,0)=Suv;
+		m(1,1)=Svv;
+
+		m=m.inverse();
+
+		b(0)=0.5*(Suuu+Suvv);
+		b(1)=0.5*(Svvv+Svuu);
+
+		//multiplying b with the inverse of m to solve the linear equation system since m*x=b so x=m⁻¹*b
+		x=m*b;
+		//calculating radius
+		r=sqrt(pow(x(0),2)+pow(x(1),2)+(Suu+Svv)/size);
+
+		//traslating into original coordinates
+		xc=x(0)+xa;
+		yc=x(1)+ya;
+
+		/////////only for dubugging
+		//publishing estimated circle
+		marker.header.frame_id = "base_footprint";
+		marker.header.stamp = ros::Time::now();
+		marker.ns = "basic_shapes";
+		marker.id = 0;
+		marker.type = visualization_msgs::Marker::CYLINDER;
+		marker.action = visualization_msgs::Marker::ADD;
+		marker.pose.position.x=xc;
+		marker.pose.position.y=yc;
+		marker.pose.position.z = 0;
+		marker.pose.orientation.x = 0.0;
+		marker.pose.orientation.y = 0.0;
+		marker.pose.orientation.z = 0.0;
+		marker.pose.orientation.w = 1.0;
+		marker.scale.x=r*2;
+		marker.scale.y=r*2;
+		marker.scale.z=1;
+		marker.scale.z = 0.0;
+		marker.color.r = 0.0f;
+		marker.color.g = 1.0f;
+		marker.color.b = 0.0f;
+		marker.color.a = 0.5;
+		marker.lifetime = ros::Duration();
+		//ROS_INFO("x: %f, y: %f",x(0),x(1));
+		///////
+
+	}
+
+void checkroadblockage(void)
+	{
+		ros::Duration Timedif;
+		geometry_msgs::TransformStamped transformStamped;
+		tf2_ros::Buffer tfBuffer;
+		tf2_ros::TransformListener tfListener(tfBuffer);
+
+		bool obstacleright=false;
+		bool obstacleleft=false;
+		//furthestpoint=sqrt(pow(LeftLine.points.back().x,2)+pow(LeftLine.points.back().y,2));
+
+		searchradius=furthestpoint+robot_diameter;
+
+		if(scan.size()>0)
+		{
+			Timedif=ros::Time::now()-scan.at(0).header.stamp;
+		}
+		else {
+			//ROS_INFO("Road Free");
+			removeblockage=false;
+			Timedif=ros::Duration(1);
+		}
+		//check if the scanned point is infront of the robot and if it is in a reasonable timeframe (not older thaen 500ms)
+		if(Timedif.toSec()<=0.5){
+			for(int i=0; i<scan.size();i++){
+				if(scan.at(0).point.x>0){
+					geometry_msgs::PointStamped Test;
+					Test.header.frame_id=scan.at(i).header.frame_id;
+					Test.header.stamp=scan.at(i).header.stamp;
+					Test.point.x=scan.at(i).point.x;
+					Test.point.y=scan.at(i).point.y;
+					Test.point.z=scan.at(i).point.z;
+					try{
+						//transform scanned point into the frame of the road detection then compare it with the road
+						transformStamped = tfBuffer.lookupTransform( roadframe,Test.header.frame_id,Test.header.stamp,ros::Duration(1));
+
+						tf2::doTransform(Test,Test, transformStamped);
+						pub.publish(Test);
+						//check whether the point is in the lane by projecting it on to the 3 roadlines and comparing their y values
+						//exact calculation not necessary since road makes wide curves
+						if(polynomial(Test.point.x, LeftLine.polynomial.a)>Test.point.y && polynomial(Test.point.x, MiddleLine.polynomial.a)<Test.point.y)
+						{
+							obstacleleft=true;
+						}
+						else if(polynomial(Test.point.x, RightLine.polynomial.a)<Test.point.y && polynomial(Test.point.x, MiddleLine.polynomial.a)>Test.point.y)
+						{
+							obstacleright=true;
+						}
+					}
+					catch(tf2::TransformException &ex)
+					{
+						ROS_WARN("%s", ex.what());
+					}
+				}
+			}
+			if(obstacleleft&&!obstacleright)
+			{
+				ROS_INFO("LeftLane Blocked");
+				removeblockage=false;
+			}
+			else if(!obstacleleft&&obstacleright)
+			{
+				ROS_INFO("RightLane Blocked");
+				removeblockage=true;
+			}
+			else if(!obstacleleft&&!obstacleright)
+			{
+				ROS_INFO("Road Free");
+				removeblockage=false;
+			}
+		}
+	}
 double aproxroadangle(void)
 	{
 	//approximate the road width based on the angle of the road relative to the robots direction
@@ -123,49 +285,14 @@ void constructgoal(road_detection::Line &Line)
 		goal.target_pose.pose.orientation.w=myQuaternion.getW();
 	}
 
-geometry_msgs::Point getPointTransform(std::string frame, ros::Time &Time)
-	{
 
-	tf2_ros::Buffer tfBuffer;
-	tf2_ros::TransformListener tfListener(tfBuffer);
-	geometry_msgs::TransformStamped transform;
-	geometry_msgs::Point PointT;
-	try{
-
-	  transform = tfBuffer.lookupTransform(frame,frame,Time);
-	  PointT.x=transform.transform.translation.x;
-	  PointT.y=transform.transform.translation.y;
-	  PointT.z=transform.transform.translation.z;
-	  return PointT;
-	}
-	catch (tf2::TransformException &ex) {
-	  ROS_WARN("%s",ex.what());
-	  ros::Duration(1.0).sleep();
-	}
-
-
-	}
 class Listener
 	{
 	public:
 		void roadCallback(const road_detection::RoadConstPtr& road)
 		{
-			bool obstacleright=false;
-			bool obstacleleft=false;
-			float xobsleftmin=searchradius;
-			float xobsleftmax;
-			float xobsrightmin=searchradius;
-			float xobsrightmax;
-			geometry_msgs::Point Scan;
-			geometry_msgs::Point LeftPoint;
-			geometry_msgs::Point MiddlePoint;
-			geometry_msgs::Point RightPoint;
-			ros::Duration Timedif;
-			geometry_msgs::TransformStamped transformStamped;
-			tf2_ros::Buffer tfBuffer;
-			tf2_ros::TransformListener tfListener(tfBuffer);
-
-			//DataSync
+			roadframe=road->header.frame_id.c_str();
+			//DataSync so each Line has always the newest data
 			if(road->laneLeft.points.size()>0)
 			{
 				LeftLane=road->laneLeft;
@@ -188,173 +315,6 @@ class Listener
 				MiddleLine=road->lineMiddle;
 			}
 			roadangle=aproxroadangle();
-
-			furthestpoint=sqrt(pow(LeftLine.points.back().x,2)+pow(LeftLine.points.back().y,2));
-			searchradius=furthestpoint+robot_diameter;
-			road_detection::Line Line;
-
-			if(scan.size()>0)
-			{
-				Timedif=ros::Time::now()-scan.at(0).header.stamp;
-			}
-			else {
-				//ROS_INFO("Road Free");
-				removeblockage=false;
-				Line=RightLane;
-				Timedif=ros::Duration(1);
-			}
-
-			//check if the scanned point is infront of the robot and if it is in a reasonable timeframe (not older thaen 500ms)
-			if(Timedif.toSec()<=0.5){
-				for(int i=0; i<scan.size();i++){
-					if(scan.at(0).point.x>0){
-						geometry_msgs::PointStamped Test;
-						Test.header.frame_id=scan.at(i).header.frame_id;
-						Test.header.stamp=scan.at(i).header.stamp;
-						Test.point.x=scan.at(i).point.x;
-						Test.point.y=scan.at(i).point.y;
-						Test.point.z=scan.at(i).point.z;
-						try{
-							//transform scanned point into the frame of the road detection then compare it with the road
-							transformStamped = tfBuffer.lookupTransform( road->header.frame_id,Test.header.frame_id,Test.header.stamp,ros::Duration(1));
-
-							tf2::doTransform(Test,Test, transformStamped);
-							pub.publish(Test);
-							//check whether the point is in the lane by projecting it on to the 3 roadlines and comparing their y values
-							//exact calculation not necessary since road makes wide curves
-							if(polynomial(Test.point.x, LeftLine.polynomial.a)>Test.point.y && polynomial(Test.point.x, MiddleLine.polynomial.a)<Test.point.y)
-							{
-								obstacleleft=true;
-							}
-							else if(polynomial(Test.point.x, RightLine.polynomial.a)<Test.point.y && polynomial(Test.point.x, MiddleLine.polynomial.a)>Test.point.y)
-							{
-								obstacleright=true;
-							}
-						}
-						catch(tf2::TransformException &ex)
-						{
-							ROS_WARN("%s", ex.what());
-						}
-					}
-				}
-				if(obstacleleft&&!obstacleright)
-				{
-					ROS_INFO("LeftLane Blocked");
-					Line=RightLane;
-					removeblockage=false;
-	//				constructgoal(Line);
-				}
-
-				else if(!obstacleleft&&obstacleright)
-				{
-					ROS_INFO("RightLane Blocked");
-					Line=LeftLane;
-					removeblockage=true;
-	//				constructgoal(Line);
-				}
-				else if(!obstacleleft&&!obstacleright)
-				{
-					ROS_INFO("Road Free");
-					removeblockage=false;
-					Line=RightLane;
-				}
-			}
-
-			//least square circle approximation as discribed by Randy Bullock in his Least-Squares Circle Fit Paper
-			//used to predict the upcoming road that cannot be seen by the camera
-			MatrixXd m(2,2);
-			Vector2d b;
-			Vector2d x;
-
-			//defining sums
-			double Suuu,Svvv,Suvv,Svuu,Suv,Suu,Svv=0;
-			double u,v,xa,ya=0;
-			double r=0;
-			//first the averages will be build
-			for (int i=0;i<LeftLine.points.size();i++)
-			{
-				xa+=LeftLine.points.at(i).x;
-				ya+=LeftLine.points.at(i).y;
-			}
-			xa=xa/LeftLine.points.size();
-			ya=ya/LeftLine.points.size();
-
-			//new the Sums need to be calculated
-			for (int i=0;i<LeftLine.points.size();i++)
-			{
-				u=LeftLine.points.at(i).x-xa;
-				v=LeftLine.points.at(i).y-ya;
-
-				Suu+=u*u;
-				Svv+=v*v;
-				Suv+=u*v;
-				Suvv+=u*v*v;
-				Svuu+=v*u*u;
-				Suuu+=u*u*u;
-				Svvv+=v*v*v;
-			}
-			m(0,0)=Suu;
-			m(0,1)=Suv;
-			m(1,0)=Suv;
-			m(1,1)=Svv;
-
-			m=m.inverse();
-
-			b(0)=0.5*(Suuu+Suvv);
-			b(1)=0.5*(Svvv+Svuu);
-
-			//multiplying b with the inverse of m to solve the linear equation system
-			x=m*b;
-			//calculating radius
-			r=sqrt(pow(x(0),2)+pow(x(1),2)+(Suu+Svv)/LeftLine.points.size());
-
-			//traslating into original coordinates
-			x(0)=x(0)+xa;
-			x(1)=x(1)+ya;
-
-			/////////only for dubugging
-			//publishing estimated circle
-
-
-		    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-		    marker.header.frame_id = "base_footprint";
-		    marker.header.stamp = ros::Time::now();
-
-		    // Set the namespace and id for this marker.  This serves to create a unique ID
-		    // Any marker sent with the same namespace and id will overwrite the old one
-		    marker.ns = "basic_shapes";
-		    marker.id = 0;
-
-		    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-
-
-		    marker.type = visualization_msgs::Marker::CYLINDER;
-
-		    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-		    marker.action = visualization_msgs::Marker::ADD;
-			marker.pose.position.x=x(0);
-			marker.pose.position.y=x(1);
-			marker.pose.position.z = 0;
-			marker.pose.orientation.x = 0.0;
-			marker.pose.orientation.y = 0.0;
-			marker.pose.orientation.z = 0.0;
-			marker.pose.orientation.w = 1.0;
-			marker.scale.x=r*2;
-			marker.scale.y=r*2;
-			marker.scale.z=1;
-			marker.scale.z = 0.0;
-		    marker.color.r = 0.0f;
-		    marker.color.g = 1.0f;
-		    marker.color.b = 0.0f;
-		    marker.color.a = 0.5;
-		    marker.lifetime = ros::Duration();
-		    //ROS_INFO("x: %f, y: %f",x(0),x(1));
-
-
-
-
-
-
 		};
 	public:
 		void scanCallback(const sensor_msgs::LaserScanConstPtr& Scan)
@@ -409,7 +369,7 @@ int main(int argc, char* argv[])
 
 	////For debugging only
 	marker_pub = n.advertise<visualization_msgs::Marker>("Circle", 1);
-
+	marker_pub2 = n.advertise<visualization_msgs::Marker>("Circle2", 1);
 
 
 
@@ -420,10 +380,14 @@ int main(int argc, char* argv[])
 	while(!ac.waitForServer(ros::Duration(5.0))){
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
-	ros::Rate r=1;
-
+	ros::Rate rate=1;
+	double x,y,r;
 	while(n.ok()){
+		checkroadblockage();
+		lastsquarecircle(LeftLine,x,y,r);
 		marker_pub.publish(marker);
+		lastsquarecircle(RightLine,x,y,r);
+		marker_pub2.publish(marker);
 		//switching left lane on and off
 		if(removeblockage)
 		{
@@ -452,7 +416,7 @@ int main(int argc, char* argv[])
 
     //TODO maybe 3 different point clouds for each line of the track
 		ros::spinOnce();
-		r.sleep();
+		rate.sleep();
 
 	}
 	return 0;
